4560b800b0c358ab336e776bb9e32cc350368ac9
twoandtwelve <jackykim573@gmail.com>
2024-07-25 02:57:24 +1200

    added weapon class to txt file, script run date

--- getWeaponData.py
+++ getWeaponData.py
@@ -1,62 +1,55 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 def parseWeapons(soup):
-
     allWeapons = {}
-    # for loop through all div tags with class 'itemlist terraria'
     for weapon in soup.find_all('div', class_='itemlist terraria'):
-
-        # check the previous h3 tag for the weapon type
         weaponType = weapon.find_previous('h3').text
-
-        # check the previous h4 tag for the stage of the game
         weaponStage = weapon.find_previous('h4').text
 
-        # If weaponType is 'Other', prepend the text of the previous h2 tag
         if weaponType == 'Others' or weaponType == 'Other':
             previous_h2 = weapon.find_previous('h2').text.strip()
             if previous_h2 == 'Melee weapons':
+                weaponClass = 'Melee'
                 weaponType = 'Melee Other'
             elif previous_h2 == 'Ranged weapons':
+                weaponClass = 'Ranged'
                 weaponType = 'Ranged Other'
             elif previous_h2 == 'Magic weapons':
+                weaponClass = 'Magic'
                 weaponType = 'Magic Other'
             elif previous_h2 == 'Summon weapons':
+                weaponClass = 'Summon'
                 weaponType = 'Summon Other'
             elif previous_h2 == 'Rogue Weapons':
+                weaponClass = 'Rogue'
                 weaponType = 'Rogue Other'
             elif previous_h2 == 'Classless weapons':
+                weaponClass = 'Classless'
                 weaponType = 'Classless Other'
+        else:
+            weaponClass = weapon.find_previous('h2').text.strip().split()[0]
 
-        # If weaponStage is 'Post Moon Lord', change it to 'Post-Moon Lord'
         if weaponStage == 'Post Moon Lord':
             weaponStage = 'Post-Moon Lord'
-
-        # If weaponStage is 'Pre Hardmode', change it to 'Pre-Hardmode'
         if weaponStage == 'Pre Hardmode':
             weaponStage = 'Pre-Hardmode'
-
-        # If weaponType is 'Boomerangs', if the previous h2 is 'Melee weapons', change it to 'Melee Boomerangs' and if the previous h2 is 'Rogue Weapons', change it to 'Rogue Boomerangs'
         if weaponType == 'Boomerangs':
             previous_h2 = weapon.find_previous('h2').text.strip()
             if previous_h2 == 'Melee weapons':
+                weaponClass = 'Melee'
                 weaponType = 'Melee Boomerangs'
             elif previous_h2 == 'Rogue Weapons':
+                weaponClass = 'Rogue'
                 weaponType = 'Rogue Boomerangs'
 
-
-        # for loop through all the a tags in the div tag
         for a in weapon.find_all('a'):
             weaponName = a.text
             weaponLink = a['href']
-
-            if weaponName:  # Check if weaponName is not an empty string
+            if weaponName:
                 weaponLink = a['href']
-
-                # make weaponName and weaponLink a tuple and append them to the allWeapons dictionary using the weaponType and weaponStage as the key
-                # for example: allWeapons[('Swords', 'Pre-Hardmode')][0] = [('Basher', '/wiki/Basher')]
-                allWeapons.setdefault((weaponType, weaponStage), []).append((weaponName, weaponLink))
+                allWeapons.setdefault((weaponType, weaponStage), []).append((weaponName, weaponClass, weaponLink))
 
     return allWeapons
 
@@ -69,50 +62,32 @@ def printWeapons(allWeapons):
     exhumedWeapons = 0
     somethingElse = 0
 
-    # loop through all the keys in allWeapons and loop through all the weapons in the lists
     for weaponType, weaponStage in allWeapons.keys():
-        for weaponName, weaponLink in allWeapons[(weaponType, weaponStage)]:
-            
-            
+        for weaponName, weaponClass, weaponLink in allWeapons[(weaponType, weaponStage)]:
             url = 'https://calamitymod.wiki.gg' + weaponLink
             response = requests.get(url)
             soup = BeautifulSoup(response.text, 'html.parser')  
 
-            # check if there is an h3 tag with the text 'Recipe'
             if soup.find('h3', string='Recipe'):
                 craftableWeapons += 1
                 print(weaponName + ' is craftable')
-                
-            # check if there is a th tag with the text 'Dropped by'
             elif soup.find('th', string='Dropped by'):
                 droppedAndChestWeapons += 1
                 print(weaponName + ' is dropped')
-
-            # check if there is a p tag that contains the text 'sold by the' or 'purchased from the'
             elif soup.find(lambda tag: tag.name == 'p' and ('sold by the' in tag.text.lower() or 'purchased from the' in tag.text.lower())):
                 purchasableWeapons += 1
                 print(weaponName + ' is purchasable')
-
-
-            # check if there is a p tag that contains the text 'fished' or 'fishing'
             elif soup.find(lambda tag: tag.name == 'p' and ('fished' in tag.text.lower() or 'fishing' in tag.text.lower())):
                 fishedWeapons += 1
                 print(weaponName + ' is fished')
-
-            # check if there is a p tag that contains the text 'exhumed'
             elif soup.find(lambda tag: tag.name == 'p' and 'exhumed' in tag.text.lower()):
                 exhumedWeapons += 1
                 print(weaponName + ' is exhumed')
-
-            
             else:
                 print(weaponName + ' something else...')
                 somethingElse += 1
-            
             numWeapons += 1
 
-
-    # print the number of weapons that are craftable, purchasable, and dropped
     print('Craftable Weapons: ' + str(craftableWeapons))
     print('Purchasable Weapons: ' + str(purchasableWeapons))
     print('Fished Weapons: ' + str(fishedWeapons))
@@ -121,30 +96,30 @@ def printWeapons(allWeapons):
     print('Something Else: ' + str(somethingElse))
     print('Total Weapons: ' + str(numWeapons))
 
+def writeWeaponsToFile(allWeapons, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        completion_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        f.write(f"Script completed at: {completion_time}\n")
+        for (weaponType, weaponStage), weapons_list in allWeapons.items():
+            for weaponName, weaponClass, weaponLink in weapons_list:
+                line = f"{weaponName} | {weaponClass} | {weaponStage} | {weaponType} | {weaponLink}\n"
+                f.write(line)
+
 #######################################################################################################
 def main():
+    start_time = datetime.now()
+    
     url = 'https://calamitymod.wiki.gg/wiki/Weapons'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-
+    
     allWeapons = parseWeapons(soup)
-
-    # Make a list of all weapon types
-    weaponTypes = list(set([weaponType for weaponType, weaponStage in allWeapons.keys()]))
-
-    # Make a list of all weapon stages
-    weaponStages = list(set([weaponStage for weaponType, weaponStage in allWeapons.keys()]))
-
-    # in allWeapons, if there does not exist a key with the weaponType and weaponStage, add an empty list
-    for weaponType in weaponTypes:
-        for weaponStage in weaponStages:
-            allWeapons.setdefault((weaponType, weaponStage), [])
-
-
-    printWeapons(allWeapons)
-
+    writeWeaponsToFile(allWeapons, 'weapons.txt')
+    
+    end_time = datetime.now()
+    duration = (end_time - start_time).total_seconds()
+    
+    print(f"Completed successfully. Runtime of {duration} s.")
 
 if __name__ == "__main__":
     main()
-
-
